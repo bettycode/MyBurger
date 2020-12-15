eeda4001df3ae87202a8dc3df8d177775e04ccd5
bettycode <betty.haile.us@gmail.com>
2020-12-14 22:22:12 -0500

    modified connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "MG@itbGcthate#1S",
   database: "burgers_db"
 });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -18,5 +26,6 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
