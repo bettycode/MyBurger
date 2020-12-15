b3f4de2029e530ac568d9bfad18ce01c1da83040
bettycode <betty.haile.us@gmail.com>
2020-12-14 23:47:53 -0500

    midified connection.js

--- config/connection.js
+++ config/connection.js
@@ -17,14 +17,14 @@ if (process.env.JAWSDB_URL){
 
 }
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 connection.connect();
 // Export connection for our ORM to use.
